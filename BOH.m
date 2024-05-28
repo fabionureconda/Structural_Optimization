b7352f890a55a89074073d9167323dd58bc536cc
fabio <fabio@nure>
2024-05-28 18:49:59 +0200

    minor changes, mostly copied in the Paz.m (changes there)

--- BOH.m
+++ BOH.m
@@ -21,10 +21,14 @@ A11 = [12  3 -6 -3;  3 12  3  0; -6  3 12 -3; -3  0 -3 12];
 A12 = [-6 -3  0  3; -3 -6 -3 -6;  0 -3 -6  3;  3 -6  3 -6];
 B11 = [-4  3 -2  9;  3 -4 -9  4; -2 -9 -4 -3;  9  4 -3 -4];
 B12 = [ 2 -3  4 -9; -3  2  9 -2;  4  9  2  3; -9 -2  3  2];
-KE = 1/(1-nu^2)/24*([A11 A12; A12' A11] + nu*[B11 B12; B12' B11]);
+
+KE = 1/(1-nu^2)/24*([A11 A12; A12' A11] + nu*[B11 B12; B12' B11]);   % Stiffness matrix with unit Young moduli
+
 nodenrs = reshape(1:(1+nelx)*(1+nely), 1+nely, 1+nelx);
 edofVec = reshape(2*nodenrs(1:end-1,1:end-1)+1, nelx*nely, 1);
-edofMat = repmat(edofVec, 1, 8) + repmat([0 1 2*nely+[2 3 0 1] -2 -1], nelx*nely, 1);
+
+edofMat = repmat(edofVec, 1, 8) + repmat([0 1 2*nely+[2 3 0 1] -2 -1], nelx*nely, 1);  % Location Matrix
+
 iK = reshape(kron(edofMat, ones(8,1))', 64*nelx*nely, 1);
 jK = reshape(kron(edofMat, ones(1,8))', 64*nelx*nely, 1);
 
