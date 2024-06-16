6c79faba34abf223d3b3ae370a5f955bda9f4b34
Fabio Nureconda <fabio.nureconda@student.kuleuven.be>
2024-06-16 16:10:21 +0200

    ok

---
+++ e3b.m
@@ -0,0 +1,132 @@
+% PROBLEM SETUP
+nelx = 100;           % Number of elements in x-direction
+nely = 40;            % Number of elements in y-direction
+volfrac = 0.12;       % Maximum volume fraction
+penal = 3;            % Penalization power
+rmin = 3;             % Filter radius in terms of elements
+ft = 2;
+
+%% MATERIAL PROPERTIES
+E0 = 210e9;
+Emin = 1e-9*E0;
+nu = 0.3;
+unitF = 10000;
+l = 0.01;
+t = 0.01;
+%% PREPARE FINITE ELEMENT ANALYSIS
+A11 = [12  3 -6 -3;  3 12  3  0; -6  3 12 -3; -3  0 -3 12];
+A12 = [-6 -3  0  3; -3 -6 -3 -6;  0 -3 -6  3;  3 -6  3 -6];
+B11 = [-4  3 -2  9;  3 -4 -9  4; -2 -9 -4 -3;  9  4 -3 -4];
+B12 = [ 2 -3  4 -9; -3  2  9 -2;  4  9  2  3; -9 -2  3  2];
+KE = 1/(1-nu^2)/24*([A11 A12;A12' A11]+nu*[B11 B12;B12' B11]);
+nodenrs = reshape(1:(1+nelx)*(1+nely),1+nely,1+nelx);
+edofVec = reshape(2*nodenrs(1:end-1,1:end-1)+1,nelx*nely,1);
+edofMat = repmat(edofVec,1,8)+repmat([0 1 2*nely+[2 3 0 1] -2 -1],nelx*nely,1);
+iK = reshape(kron(edofMat,ones(8,1))',64*nelx*nely,1);
+jK = reshape(kron(edofMat,ones(1,8))',64*nelx*nely,1);
+
+% DEFINE LOADS AND SUPPORTS (HALF MBB-BEAM)
+F1 = sparse(2*((nely+1)*(nelx)+1), 1, -unitF, 2*(nely+1)*(nelx+1), 1);
+F2 = sparse(2*((nely+1)*(nelx)+2), 1, -unitF, 2*(nely+1)*(nelx+1), 1);
+F3 = sparse(2*((nely+1)*(nelx)+3), 1, -unitF, 2*(nely+1)*(nelx+1), 1);
+F4 = sparse(2*((nely+1)*(nelx)+4), 1, -unitF, 2*(nely+1)*(nelx+1), 1);
+F5 = sparse(2*((nely+1)*(nelx)+5), 1, -unitF, 2*(nely+1)*(nelx+1), 1);
+F = F1+F2+F3+F4+F5;
+U = zeros(2*(nely+1)*(nelx+1),1);
+fixeddofs = [1:2*(nely+1)];
+alldofs = [1:2*(nely+1)*(nelx+1)];
+freedofs = setdiff(alldofs,fixeddofs);
+
+% STRESS INITIALIZATION
+Be = 1/(2*l)*[-1, 0, 1, 0, 1, 0, -1, 0; 0, -1, 0, -1, 0, 1, 0, 1; -1, -1, -1, 1, 1, 1, 1, -1];
+D0 = E0/(1-nu^2)*[1, nu, 0; nu, 1, 0; 0, 0, (1-nu)/2];
+
+%% PREPARE FILTER
+iH = ones(nelx*nely*(2*(ceil(rmin)-1)+1)^2,1);
+jH = ones(size(iH));
+sH = zeros(size(iH));
+k = 0;
+for i1 = 1:nelx
+  for j1 = 1:nely
+    e1 = (i1-1)*nely+j1;
+    for i2 = max(i1-(ceil(rmin)-1),1):min(i1+(ceil(rmin)-1),nelx)
+      for j2 = max(j1-(ceil(rmin)-1),1):min(j1+(ceil(rmin)-1),nely)
+        e2 = (i2-1)*nely+j2;
+        k = k+1;
+        iH(k) = e1;
+        jH(k) = e2;
+        sH(k) = max(0,rmin-sqrt((i1-i2)^2+(j1-j2)^2));
+      end
+    end
+  end
+end
+H = sparse(iH,jH,sH);
+Hs = sum(H,2);
+%% INITIALIZE ITERATION
+x = repmat(volfrac,nely,nelx);
+xPhys = x;
+loop = 0;
+change = 1;
+%% START ITERATION
+while change > 0.01
+  loop = loop + 1;
+  %% FE-ANALYSIS
+  sK = reshape(KE(:)*(Emin+xPhys(:)'.^penal*(E0-Emin)),64*nelx*nely,1);
+  K = sparse(iK,jK,sK); K = (K+K')/2;
+  U(freedofs) = K(freedofs,freedofs)\F(freedofs);
+  %% OBJECTIVE FUNCTION AND SENSITIVITY ANALYSIS
+  ce = reshape(sum((U(edofMat)*KE).*U(edofMat),2),nely,nelx);
+  c = sum(sum((Emin+xPhys.^penal*(E0-Emin)).*ce));
+  dc = -penal*(E0-Emin)*xPhys.^(penal-1).*ce;
+  dv = ones(nely,nelx);
+
+  %% STRESS CALCULATION
+  sig_xxe = zeros(nelx*nely, 1);
+  sig_yye = zeros(nelx*nely, 1);
+  sig_xye = zeros(nelx*nely, 1);
+
+  for el = 1:nelx*nely
+        Ue = U(edofMat(el, :));
+        sig_xxe(el) = D0(1, :) * Be * Ue;
+        sig_yye(el) = D0(2, :) * Be * Ue;
+        sig_xye(el) = D0(3, :) * Be * Ue;
+  end
+    
+  sig_vMe = sqrt(sig_xxe.^2 + sig_yye.^2 - sig_xxe .* sig_yye + 3 .* sig_xye.^2);
+  %% FILTERING/MODIFICATION OF SENSITIVITIES
+  if ft == 1
+    dc(:) = H*(x(:).*dc(:))./Hs./max(1e-3,x(:));
+  elseif ft == 2
+    dc(:) = H*(dc(:)./Hs);
+    dv(:) = H*(dv(:)./Hs);
+  end
+  %% OPTIMALITY CRITERIA UPDATE OF DESIGN VARIABLES AND PHYSICAL DENSITIES
+  l1 = 0; l2 = 1e9; move = 0.5;
+  while (l2-l1)/(l1+l2) > 1e-3
+    lmid = 0.5*(l2+l1);
+    xnew = max(0,max(x-move,min(1,min(x+move,x.*sqrt(-dc./dv/lmid)))));
+    if ft == 1
+      xPhys = xnew;
+    elseif ft == 2
+      xPhys(:) = (H*xnew(:))./Hs;
+    end
+    if sum(xPhys(:)) > volfrac*nelx*nely, l1 = lmid; else l2 = lmid; end
+  end
+  change = max(abs(xnew(:)-x(:)));
+  x = xnew;
+  %% PRINT RESULTS
+  fprintf(' It.:%5i Obj.:%11.4f Vol.:%7.3f ch.:%7.3f\n',loop,c, ...
+    mean(xPhys(:)),change);
+  %% PLOT DENSITIES
+  figure(1)
+  colormap(gray); imagesc(1-xPhys); caxis([0 1]); axis equal; axis off; drawnow;
+  %% PLOT STRESSES
+  sig_vMe = reshape(sig_vMe, nely, nelx);
+  
+  figure(2)
+  imagesc(sig_vMe);
+  axis('equal');
+  axis('off');
+  colormap('turbo');
+  colorbar;
+end
